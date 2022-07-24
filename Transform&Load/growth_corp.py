b0c0657278f5e99b8e04db8d10098e0c5f9f3b3d
MaryYoungae <97429679+MaryYoungae@users.noreply.github.com>
2022-07-25 06:22:14 +0900

    [Add] 'inudstry_market_cap' Transform&DBLoad

--- Transform&Load/growth_corp.py
+++ Transform&Load/growth_corp.py
@@ -10,6 +10,7 @@ from datetime import datetime
 from dateutil.tz import gettz
 from dateutil.relativedelta import relativedelta
 
+
 newsSchema = StructType([
     StructField("corp_name", StringType(), False),
     StructField("industry_name", StringType(), False),
@@ -22,28 +23,28 @@ corpDf = spark.read.format("csv").schema(newsSchema).load(path)
 
 # 전월 산업군별 기업 시가총액 불러오기
 path1 = "/data/collect/corperation/inudstry_market_cap_{}.csv".format((datetime.now(gettz('Asia/Seoul'))-relativedelta(months=1)).strftime("%Y%m"))
-preMonth_corpDf = spark.read.format("csv").schema(newsSchema).load(path1)
+preMonthCorp = spark.read.format("csv").schema(newsSchema).load(path1)
 
 
 # preMonth_corpDf 컬럼명 변경
-preMonth_corpDf = preMonth_corpDf.select(col('corp_name').alias('corp_name1') ,col('industry_name').alias('industry_name1'), col('maket_cap').alias('maket_cap1'))
+preMonthCorp = preMonthCorp.select(col('corp_name').alias('corp_name1') ,col('industry_name').alias('industry_name1'), col('maket_cap').alias('maket_cap1'))
 
 # 당월 & 전월 산업군별 기업 시가총액 left outer join
-joinExprssion = (corpDf["corp_name"] == preMonth_corpDf["corp_name1"]) & (corpDf["industry_name"] == preMonth_corpDf["industry_name1"])
-growth_corp = corpDf.join(preMonth_corpDf, joinExprssion, "left_outer")
+joinExprssion = (corpDf["corp_name"] == preMonthCorp["corp_name1"]) & (corpDf["industry_name"] == preMonthCorp["industry_name1"])
+growthCorp = corpDf.join(preMonthCorp, joinExprssion, "left_outer")
 
 # 전월 대비 시가총액 증가율 처리
-growth_corp = growth_corp.select('corp_name', 'industry_name', round(expr('((maket_cap-maket_cap1)/maket_cap1)*100'), 2).alias('growth_rate'))
+growthCorp = growthCorp.select('corp_name', 'industry_name', round(expr('((maket_cap-maket_cap1)/maket_cap1)*100'), 2).alias('growth_rate'))
 
 # 시가총액 증가율 순위 집계
 widowSpec = Window.partitionBy("industry_name").orderBy(desc("growth_rate"))
-growth_corp = growth_corp.withColumn("corp_rank", row_number().over(widowSpec)).where(col("corp_rank") <= 10)
+growthCorp = growthCorp.withColumn("corp_rank", row_number().over(widowSpec)).where(col("corp_rank") <= 10)
 
 # 집계 발행월 컬럼 추가
-growth_corp = growth_corp.withColumn("reg_month", lit(datetime.now(gettz('Asia/Seoul')).strftime("%Y-%m")))
+growthCorp = growthCorp.withColumn("reg_month", lit(datetime.now(gettz('Asia/Seoul')).strftime("%Y-%m")))
 
 # 기타 카테고리 제거
-growth_corp = growth_corp.select("industry_name", "reg_month", "corp_name","growth_rate", "corp_rank").where(col("industry_name") != "기타")
+growthCorp = growthCorp.select("industry_name", "reg_month", "corp_name","growth_rate", "corp_rank").where(col("industry_name") != "기타")
 
 
 # DB 연결
@@ -52,7 +53,7 @@ tablename = "growth_corp_list"
 props = {"driver":"com.mysql.cj.jdbc.Driver", "user":"user", "password": "password"}
 
 # DB 저장
-growth_corp.write.mode("append").jdbc(newPath, tablename, properties=props)
+growthCorp.write.mode("append").jdbc(newPath, tablename, properties=props)
 
 
 spark.stop()
